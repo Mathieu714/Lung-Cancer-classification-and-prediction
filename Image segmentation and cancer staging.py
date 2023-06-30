a8530c7ddca429990b5d4d4d45ba96d3b82bffeb
Mathieu714 <120245181+Mathieu714@users.noreply.github.com>
2023-06-30 16:14:05 +0800

    Update Image segmentation and cancer staging.py

--- Image segmentation and cancer staging.py
+++ Image segmentation and cancer staging.py
@@ -1,144 +1,183 @@
-import os
-import random
-import cv2
-from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
-from keras.models import Sequential, load_model
+import PIL
+import tensorflow as tf
+from keras.saving.legacy.save import load_model
+from tensorflow import keras
+from keras import layers
+import matplotlib.pyplot as plt
 import numpy as np
-
-from PIL import ImageOps
-from PIL.Image import Image
-from PIL._imaging import display
+import os
+import cv2
+from sklearn.model_selection import train_test_split
 from keras.preprocessing.image import ImageDataGenerator
-from keras.utils import load_img
 
-input_dir = 'Full Dataset/Image segmentation data/Image/'
-target_dir = 'Full Dataset/Image segmentation data/Annotations/'
-num_classes = 3
-batch_size = 32
-img_size = (160, 160)
+
+
+#loading data and preprocessing
+image_dir = 'D:\Mathieu\Program Files\AARD Research\Full Dataset\Image segmentation data\image'
+mask_dir = 'D:\Mathieu\Program Files\AARD Research\Full Dataset\Image segmentation data\masks'
+img_size = 256
 
 categories = ['Image', 'Annotations']
-data_input = []
-for file in os.listdir(input_dir):
-   filepath = os.path.join(input_dir, file)
+input_image = []
+for file in os.listdir(image_dir):
+   filepath = os.path.join(image_dir, file)
    img_array = cv2.imread(filepath, cv2.IMREAD_COLOR)
-   new_array = cv2.resize(img_array, img_size)
-   data_input.append([new_array])
+   new_array = cv2.resize(img_array, (img_size, img_size))
+   input_image.append([new_array])
 
-data_annotations = []
-for file in os.listdir(target_dir):
-   filepath = os.path.join(target_dir, file)
+input_mask = []
+for file in os.listdir(mask_dir):
+   filepath = os.path.join(mask_dir, file)
    img_array = cv2.imread(filepath, cv2.IMREAD_COLOR)
-   new_array = cv2.resize(img_array, img_size)
-   data_annotations.append([new_array])
+   new_array = cv2.resize(img_array, (img_size, img_size))
+   input_mask.append([new_array])
 
-data_gen_args = dict(featurewise_center=True,
-                     featurewise_std_normalization=True,
-                     rotation_range=90,
-                     width_shift_range=0.1,
-                     height_shift_range=0.1,
-                     zoom_range=0.2)
-image_datagen = ImageDataGenerator(**data_gen_args)
-mask_datagen = ImageDataGenerator(**data_gen_args)
-# Provide the same seed and keyword arguments to the fit and flow methods
-seed = 1
-data_input = np.array(data_input).reshape(-1, 160, 160, 3)
-data_annotations = np.array(data_annotations).reshape(-1, 160, 160, 3)
+input_image = np.array(input_image).reshape(-1, img_size, img_size, 3)
+input_image = input_image / input_image.max()
+input_mask = np.array(input_mask).reshape(-1, img_size, img_size, 3)
+input_mask = input_mask / input_mask.max()
+X_train_dataset, X_test_dataset, y_train_dataset, y_test_dataset = train_test_split(input_image, input_mask, random_state=1000) #train test split
+X_train_dataset = X_train_dataset.reshape(X_train_dataset.shape[0], img_size, img_size, 3)
+X_test_dataset = X_test_dataset.reshape(X_test_dataset.shape[0], img_size, img_size, 3)
+y_train_dataset = y_train_dataset.reshape(y_train_dataset.shape[0], img_size, img_size, 3)
+y_test_dataset = y_test_dataset.reshape(y_test_dataset.shape[0], img_size, img_size, 3)
 
-image_datagen.fit(data_input, augment=True, seed=seed)
-mask_datagen.fit(data_annotations, augment=True, seed=seed)
-image_generator = image_datagen.flow_from_directory(
-    input_dir,
-    class_mode=None,
-    seed=seed)
-mask_generator = mask_datagen.flow_from_directory(
-    target_dir,
-    class_mode=None,
-    seed=seed)
 
-from keras import layers
-import keras
 
-def get_model(img_size, num_classes):
-    inputs = keras.Input(shape=(img_size) + (3,))
 
-    ### [First half of the network: downsampling inputs] ###
+#image data generator and flow
+seed=1337
 
-    # Entry block
-    x = layers.Conv2D(32, 3, strides=2, padding="same")(inputs)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
+train_datagen = ImageDataGenerator(rotation_range=360,
+                                   width_shift_range=0.15,
+                                   height_shift_range=0.15,
+                                   horizontal_flip=True,
+                                   vertical_flip=True
+                                   )
+test_datagen = ImageDataGenerator(rotation_range=360,
+                                   width_shift_range=0.15,
+                                   height_shift_range=0.15,
+                                   horizontal_flip=True,
+                                   vertical_flip=True
+                                  )
 
-    previous_block_activation = x  # Set aside residual
+train_datagen.fit(X_train_dataset, augment=True, seed=seed)
+test_datagen.fit(X_test_dataset, augment=True, seed=seed)
 
-    # Blocks 1, 2, 3 are identical apart from the feature depth.
-    for filters in [64, 128, 256]:
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
+train_gen = train_datagen.flow(
+    X_train_dataset,
+    y_train_dataset,
+    batch_size=1024,
+    shuffle=True,
+    seed=seed
+)
+test_gen = test_datagen.flow(
+    X_test_dataset,
+    y_test_dataset,
+    batch_size=1024,
+    shuffle=True,
+    seed=seed
+)
 
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
 
-        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
 
-        # Project residual
-        residual = layers.Conv2D(filters, 1, strides=2, padding="same")(
-            previous_block_activation
-        )
-        x = layers.add([x, residual])  # Add back residual
-        previous_block_activation = x  # Set aside next residual
+#creating model
+def double_conv_block(x, n_filters):
+   # Conv2D then ReLU activation
+   x = layers.Conv2D(n_filters, 3, padding = "same", activation = "relu", kernel_initializer = "he_normal")(x)
+   # Conv2D then ReLU activation
+   x = layers.Conv2D(n_filters, 3, padding = "same", activation = "relu", kernel_initializer = "he_normal")(x)
+   return x
 
-    ### [Second half of the network: upsampling inputs] ###
+def downsample_block(x, n_filters):
+   f = double_conv_block(x, n_filters)
+   p = layers.MaxPool2D((2,2), padding='same')(f)
+   p = layers.Dropout(0.3)(p)
 
-    for filters in [256, 128, 64, 32]:
-        x = layers.Activation("relu")(x)
-        x = layers.Conv2DTranspose(filters, 1, padding="same")(x)
-        x = layers.BatchNormalization()(x)
+   return f, p
 
-        x = layers.Activation("relu")(x)
-        x = layers.Conv2DTranspose(filters, 1, padding="same")(x)
-        x = layers.BatchNormalization()(x)
+def upsample_block(x, conv_features, n_filters):
+   # upsample
+   x = layers.Conv2DTranspose(n_filters, 3, 2, padding="same")(x)
+   # concatenate
+   x = layers.concatenate([x, conv_features])
+   # dropout
+   x = layers.Dropout(0.3)(x)
+   # Conv2D twice with ReLU activation
+   x = double_conv_block(x, n_filters)
 
-        x = layers.UpSampling2D(2)(x)
+   return x
 
-        # Project residual
-        residual = layers.UpSampling2D(2)(previous_block_activation)
-        residual = layers.Conv2D(filters, 1, padding="same")(residual)
-        x = layers.add([x, residual])  # Add back residual
-        previous_block_activation = x  # Set aside next residual
+def build_unet_model():
+ # inputs
+   inputs = layers.Input(shape=(256,256,3))
 
-    # Add a per-pixel classification layer
-    outputs = layers.Conv2D(num_classes, 3, activation="softmax", padding="same")(x)
+   # encoder: contracting path - downsample
+   # 1 - downsample
+   f1, p1 = downsample_block(inputs, 64)
+   # 2 - downsample
+   f2, p2 = downsample_block(p1, 128)
+   # 3 - downsample
+   f3, p3 = downsample_block(p2, 256)
+   # 4 - downsample
+   f4, p4 = downsample_block(p3, 512)
 
-    # Define the model
-    model = keras.Model(inputs, outputs)
-    return model
+   # 5 - bottleneck
+   bottleneck = double_conv_block(p4, 1024)
 
-model = get_model(img_size, num_classes)
+   # decoder: expanding path - upsample
+   # 6 - upsample
+   u6 = upsample_block(bottleneck, f4, 512)
+   # 7 - upsample
+   u7 = upsample_block(u6, f3, 256)
+   # 8 - upsample
+   u8 = upsample_block(u7, f2, 128)
+   # 9 - upsample
+   u9 = upsample_block(u8, f1, 64)
+
+   # outputs
+   outputs = layers.Conv2D(3, 1, padding="same", activation = "softmax")(u9)
+
+   # unet model with Keras Functional API
+   unet_model = tf.keras.Model(inputs, outputs, name="U-Net")
+
+   return unet_model
+
+
+#get model
+model = build_unet_model()
+
+
+#complie and summary
+model.compile(optimizer=tf.keras.optimizers.Adam(),
+                  loss="categorical_crossentropy",
+                  metrics="accuracy")
+
 model.summary()
-# Free up RAM in case the model definition cells were run multiple times
-keras.backend.clear_session()
 
-import random
 
-# Split our img paths into a training and a validation set
-val_samples = 1000
-random.Random(1337).shuffle(data_input)
-random.Random(1337).shuffle(data_annotations)
-train_input_img_paths = data_input[:-val_samples]
-train_target_img_paths = data_annotations[:-val_samples]
-val_input_img_paths = data_input[-val_samples:]
-val_target_img_paths = data_annotations[-val_samples:]
 
-# Instantiate data Sequences for each split
-train_gen = zip(image_generator, mask_generator)
+#train model
+model_history = model.fit(train_gen,
+                              epochs=3,
+                              validation_data=test_gen)
+model.save('image_segmentation.h5')
 
-model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy")
 
-# Train the model, doing validation at the end of each epoch.
-epochs = 3
-model.fit(train_gen, steps_per_epoch=2000, epochs=epochs)
 
-model.save('img_seg_model')
+#mask prediction
+model = load_model('image_segmentation.h5')
+predicted_mask = model.predict(X_test_dataset)
+plt.imsave('predicted_mask.png', predicted_mask)
+
+plt.subplot(131)
+plt.title('image')
+plt.imshow(input_image[0])
+plt.subplot(132)
+plt.title('predicted_mask')
+plt.imshow(cv2.imread('predicted_mask.png'))
+plt.subplot(133)
+plt.title('true mask')
+plt.imshow(input_mask[0])
+plt.savefig('image_segmentation_result')
+plt.show()
